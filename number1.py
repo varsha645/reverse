2a2126e845f0eaa4c9c5cf88e4506db29c6f4c9a
Varsha <ponnu2thampachan@gmail.com>
2020-05-12 21:00:18 +0400

    first commit

---
+++ number1.py
@@ -0,0 +1,11 @@
+def Reverse(lst): 
+    return [ele for ele in reversed(lst)] 
+      
+lst = [10, 11, 12, 13, 14, 15] 
+print(Reverse(lst))
+
+def Reverse(lst): 
+    return [ele for ele in reversed(lst)] 
+      
+lst = [4, 5, 6, 7, 8, 9] 
+print(Reverse(lst))
